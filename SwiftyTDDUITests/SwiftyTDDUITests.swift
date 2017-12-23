fbd0afa30bb1d760794e908e51621585c7911b5c
Bill Thompson <wthomps@willandnora.com>
2017-12-22 21:49:24 -0800

    ok so this represents a better than record edit.  adding a tableview entry, viewing it, then deleting it by editing the recorded content.

--- SwiftyTDDUITests/SwiftyTDDUITests.swift
+++ SwiftyTDDUITests/SwiftyTDDUITests.swift
@@ -30,8 +30,8 @@ class SwiftyTDDUITests: XCTestCase {
     
     func testDriveDesign() {
         //first test is title, in TDD we write the test first then change things to match till the test passes.
-        print(app.debugDescription)
-        let navBar = app.navigationBars.element.identifier
+        print(self.app.debugDescription)
+        let navBar = self.app.navigationBars.element.identifier
         XCTAssert(navBar == "SwiftyTDD", "App Title does not match intended App Name")
         
        //the following was a record to get my bearings.
@@ -40,12 +40,14 @@ class SwiftyTDDUITests: XCTestCase {
         swiftytddNavigationBar.buttons["Add"].tap()
         
         let tablesQuery = app.tables
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["2017-12-23 05:40:25 +0000"]/*[[".cells.staticTexts[\"2017-12-23 05:40:25 +0000\"]",".staticTexts[\"2017-12-23 05:40:25 +0000\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery.cells.firstMatch.tap()
+        
         app.navigationBars["Detail"].buttons["SwiftyTDD"].tap()
-        swiftytddNavigationBar.buttons["Edit"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Delete 2017-12-23 05:40:25 +0000"]/*[[".cells.buttons[\"Delete 2017-12-23 05:40:25 +0000\"]",".buttons[\"Delete 2017-12-23 05:40:25 +0000\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+ 
+        //swiftytddNavigationBar.buttons["Edit"].tap()
+        tablesQuery.cells.firstMatch.swipeLeft()
         tablesQuery.buttons["Delete"].tap()
-        swiftytddNavigationBar.buttons["Done"].tap()
+        //swiftytddNavigationBar.buttons["Done"].tap()
         
     }
     
